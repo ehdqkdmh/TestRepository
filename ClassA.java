ed65e79f706904c9a1615e4a29cd077b08a01141
Shin YongKwon <blueskii@naver.com>
2017-04-24 14:08:00 +0900

    연습 파일 삭제

--- ClassA.java
+++
@@ -1,6 +0,0 @@
-public class ClassA {
-  public static void main(String[] args) {
-    System.out.println("user1 작성");
-    System.out.println("user2 작성");
-  }
-}
--- ClassB.java
+++
@@ -1,5 +0,0 @@
-public class ClassB {
-  public static void main(String[] args) {
-    System.out.println("user2가 작성");
-  }
-}
