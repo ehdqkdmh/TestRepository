f20c34c8bf7fd085e586e892ef9acb2d9fd5bd9e
Shin YongKwon <blueskii@naver.com>
2017-04-24 12:32:38 +0900

    수정함

--- ClassA.java
+++ ClassA.java
@@ -1,7 +1,5 @@
 public class ClassA {
   public static void main(String[] args) {
-    System.out.println("user1이 작성");
-    System.out.println("user2가 작성");
-    System.out.println("user2가 작성2");
+
   }
 }
