a52dcd0e317a157b12a7e01aefb8133356fb6f7e
Shin YongKwon <blueskii@naver.com>
2017-05-30 15:56:14 +0900

    프로젝트 수정

--- SpringWebProgramming/src/com/mycompany/myapp/dao/Exam10Dao.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.myapp.dao;
-
-public interface Exam10Dao {
-	
-	public void insert();
-	
-	public void select();
-	
-}
--- SpringWebProgramming/src/com/mycompany/myapp/service/Exam10Service.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.myapp.service;
-
-public interface Exam10Service {
-
-	public void join();
-	
-	public void login();
-	
-}
