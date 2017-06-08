47bcd445cb90402837635f21916f699197b61af6
Shin YongKwon <blueskii@naver.com>
2017-06-08 15:40:16 +0900

    WebApplication 프로젝트 수정

--- Hardware/src/test/PhotoresistorSensorRgbLedTest.java
+++ Hardware/src/test/PhotoresistorSensorRgbLedTest.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
-/**
- *
- * @author Administrator
- */
+import com.pi4j.io.gpio.RaspiPin;
+import hardware.converter.PCF8591;
+import hardware.led.RgbLedDigital;
+import hardware.sensor.PhotoresistorSensor;
+
 public class PhotoresistorSensorRgbLedTest {
+	public static void main(String[] args) throws Exception {
+		RgbLedDigital rgbLedDigital= new RgbLedDigital(RaspiPin.GPIO_00, RaspiPin.GPIO_02, RaspiPin.GPIO_03);
+		PCF8591 pcf8591= new PCF8591(0x48, PCF8591.AIN0);
+		PhotoresistorSensor photoresistorSensor= new PhotoresistorSensor(pcf8591);
+		
+		while(true){
+			double value=photoresistorSensor.getValue();
 	
+			if(value>100){
+				rgbLedDigital.green(true);
+			}else if(value<=100){
+				rgbLedDigital.green(false);
+				rgbLedDigital.red(true);
+			}
+			System.out.println(value + "도");
+			Thread.sleep(1000);
+		}
+		
+	}
 }
